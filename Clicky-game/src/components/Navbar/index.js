76f4e88f959966e09a5281290ecd3996a6a53fd9
Eric Hamil <erichamil@Erics-MacBook-Pro.local>
2020-01-18 12:59:49 -0500

    final changes

---
+++ Clicky-game/src/components/Navbar/index.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./style.css";
+
+function Navbar(props) {
+    return (
+
+        <nav className="navbar">
+            <ul>
+                <li>
+                    <a className="title" href="/">Star Wars Clicky Game</a>
+                </li>
+                <li>{props.message}</li>
+                <li>Score: {props.score} | TopScore: {props.topScore}</li>
+            </ul>
+        </nav>
+    );
+}
+
+export default Navbar;
